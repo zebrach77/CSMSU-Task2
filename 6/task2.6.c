b9dc7821fb82f388a4721a78b0ed35d4e5068f89
PRAC-CSMSU <zebrach77@gmail.com>
2021-09-22 19:29:25 +0300

    Added attention comment to task 6 It's not done yet!!!

--- 6/task2.6.c
+++ 6/task2.6.c
@@ -1,4 +1,9 @@
+//ATTENTION: This task is'nt done yet!!!
+//ATTENTION: This task is'nt done yet!!!
+//ATTENTION: This task is'nt done yet!!!
+//ATTENTION: This task is'nt done yet!!!
 #include <stdio.h>
+#include <stdlib.h>
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
 struct tree_node
@@ -7,7 +12,7 @@ struct tree_node
   int elem;
 };
 typedef struct tree_node* tree;
-
+tree tr1;
 void init(tree tr, int val)
 {
   tr->left = NULL;
@@ -33,12 +38,44 @@ void add_node(tree tr, int val){
 
 void remove_tree(tree tr)
 {
-  if(tr->left==NULL)
+  if(tr->left==NULL && tr->right==NULL)
   {
-    
+    free(tr);
+  }
+  else if(tr->left)
+    remove_tree(tr->left);
+  else if(tr->right)
+    remove_tree(tr->right);
+}
+
+tree find_elem_and_return(tree tr, int val)
+{
+  if (!tr)
+  {
+    fprintf(stderr, RED "%s\n" RESET, "Element not found");
+    return tr;
+  }
+  if(val < tr->elem)
+  {
+    return find_elem_and_return(tr->left, val);
+  }
+  else if(val > tr->elem)
+  {
+    return find_elem_and_return(tr->right, val);
   }
   else
   {
-
+    return tr;
   }
+}
+
+void remove
+
+int main()
+{
+  add_node(tr1, 34);
+  add_node(tr1, 56);
+  add_node(tr1, 12);
+  add_node(tr1, 33);
+  add_node(tr1, 12);
 }
